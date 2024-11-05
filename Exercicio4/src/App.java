5b29248ca77d49f411de74424d3b54d4fcff1a39
Arthur Marciano <arthur.martep@gmail.com>
2024-11-05 15:15:15 -0300

    Atualizando o .gitignore para ignorar arquivos .class

---
+++ Exercicio4/src/App.java
@@ -0,0 +1,12 @@
+/* 4. Sistema de Gestão de Alunos com Polimorfismo e Streams API
+Implemente um sistema para gerenciar alunos de diferentes cursos universitários, 
+como Ciência da Computação, Engenharia, e Direito. Cada curso terá um método específico para calcular 
+a média ponderada das notas de seus alunos, baseado em disciplinas diferentes, com pesos variados para cada curso.*/
+
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
